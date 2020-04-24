e46c500a8d9f6b2721c81990c35c71258c3b6f9d
Navid Khalili <nkhalili@live.com>
2020-04-24 19:38:21 -0400

    Added Products db.

--- ECommerce.Api.Products/Startup.cs
+++ ECommerce.Api.Products/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ECommerce.Api.Products.Db;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ECommerce.Api.Products
 {
@@ -24,6 +20,11 @@ namespace ECommerce.Api.Products
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ProductsDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("Products");
+            });
+
             services.AddControllers();
         }
 
