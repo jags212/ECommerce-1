e33fa83d6e7bc103e66d38487e36245bd243bc1c
Navid Khalili <nkhalili@live.com>
2020-04-24 23:09:01 -0400

    Added Products API.

---
+++ ECommerce.Api.Products/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Api.Products.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsProvider productsProvider;
+
+        public ProductsController(IProductsProvider productsProvider)
+        {
+            this.productsProvider = productsProvider;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductsAsync()
+        {
+            var result = await productsProvider.GetProductsAsync();
+            if (result.IsSuccess)
+            {
+                return Ok(result.Products);
+            }
+
+            return NotFound();
+        }
+    }
+}
