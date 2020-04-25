79f6897efb2ddf5b1783996ccdaa5442603713d6
Navid Khalili <nkhalili@live.com>
2020-04-25 11:26:45 -0400

    Added get product by id

--- ECommerce.Api.Products/Controllers/ProductsController.cs
+++ ECommerce.Api.Products/Controllers/ProductsController.cs
@@ -30,5 +30,17 @@ namespace ECommerce.Api.Products.Controllers
 
             return NotFound();
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductAsync(int id) 
+        {
+            var result = await productsProvider.GetProductAsync(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Product);
+            }
+
+            return NotFound();
+        }
     }
 }
