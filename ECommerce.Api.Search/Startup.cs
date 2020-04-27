8fb8d5693546a766e8a17737e178f443b54ffd1a
Navid <nkhalili@live.com>
2020-04-27 10:29:39 -0400

    Added Customers microservice (dynamic type).

--- ECommerce.Api.Search/Startup.cs
+++ ECommerce.Api.Search/Startup.cs
@@ -25,6 +25,7 @@ namespace ECommerce.Api.Search
             services.AddScoped<ISearchService, SearchService>();
             services.AddScoped<IOrdersService, OrdersService>();
             services.AddScoped<IProductsService, ProductsService>();
+            services.AddScoped<ICustomersService, CustomersService>();
 
             services.AddHttpClient("OrdersService", config =>
             {
@@ -37,6 +38,13 @@ namespace ECommerce.Api.Search
                         config.BaseAddress = new System.Uri(Configuration["Services:Products"]);
                     })
                 .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+
+            services
+                .AddHttpClient("CustomersService", config =>
+                {
+                    config.BaseAddress = new Uri(Configuration["Services:Customers"]);
+                })
+                .AddTransientHttpErrorPolicy(c => c.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
 
             services.AddControllers();
         }
